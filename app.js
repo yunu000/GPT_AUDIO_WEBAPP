fb600e0e12cd4ada86821e95c3375accfd6f71f9
Yash Pandey <yash.pandey1687@gmail.com>
2023-06-26 18:50:01 +0530

    CORS ADDED

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express=require('express');
 const app=express();
+const cors = require('cors')
+
 require('dotenv').config()
 const {Configuration,OpenAIApi} = require('openai')
 
 
+const allowedOrigins = ['https://getyouranswer.onrender.com'];
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+
+}));
 //setting api key for using OpenAi api
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
